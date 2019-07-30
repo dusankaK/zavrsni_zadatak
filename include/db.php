112cf6095aa5c5b154177344a3b0cc8781fa6814
Ime Prezime <moj_email@gmail.com>
2019-07-30 21:48:12 +0200

    Single post izmena

--- include/db.php
+++ include/db.php
@@ -18,12 +18,14 @@ catch(PDOException $e){
     function database($sql, $connection, $fetch){
         $sql;
         $statement = $connection->prepare($sql);
+        //$statement->bindParam(':id', $id);
         //izvrsavam upit
         $statement->execute();
         
         $statement->setFetchMOde(PDO::FETCH_ASSOC);
 
         $results = $statement->fetchAll();//daj mi rezultate koje si dobio, kad smo setovali fetch mode
+        
         return $results;
     }
 ?>
--- posts.php
+++ posts.php
@@ -20,7 +20,7 @@
     ?>
 
         <div class="blog-post">
-            <a href="single-post.php?post_id=<?php echo $post['id']; ?>"><h2 class="blog-post-title"><?php echo $post['title']; ?></h2></a>
+            <a href="single-post.php?id=<?php echo $post['id']; ?>"><h2 class="blog-post-title"><?php echo $post['title']; ?></h2></a>
             <p class="blog-post-meta"><?php echo $post['created_at']. " "; ?> by <a href="#"><?php echo " " . $post['author']; ?></a></p>
 
             <hr>
